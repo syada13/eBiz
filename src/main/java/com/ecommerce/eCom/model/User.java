3e55f55e6672704b496f58941b5faadcf6eb8b1c
Suresh Yadav <suresh23_himcs@rediffmail.com>
2025-07-08 20:38:31 +0530

    Managing users and roles

---
+++ src/main/java/com/ecommerce/eCom/model/User.java
@@ -0,0 +1,58 @@
+package com.ecommerce.eCom.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name="users",
+      uniqueConstraints = {
+        @UniqueConstraint(columnNames = "usernames"),@UniqueConstraint(columnNames = "email")
+      })
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="user_id")
+    private Long userId;
+
+    @NotBlank
+    @Size(max=20)
+    @Column(name ="username")
+    private String userName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    @Column(name ="email")
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Setter
+    @Getter
+    @ManyToMany(cascade={CascadeType.PERSIST,CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+              joinColumns = @JoinColumn(name ="user_id"),
+              inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+}
