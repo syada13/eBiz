c30be4db78ea7707586daed556ba6db46b1958ca
Suresh Yadav <suresh23_himcs@rediffmail.com>
2025-07-09 10:20:53 +0530

    User addresse implementation

--- src/main/java/com/ecommerce/eCom/model/Address.java
+++ src/main/java/com/ecommerce/eCom/model/Address.java
@@ -1,14 +1,56 @@
 package com.ecommerce.eCom.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "addresses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
+
+    @NotBlank
+    @Size(min = 5, message =" Street must be atleast 5 characters")
+    private String street;
+
+    @NotBlank
+    @Size(min = 5, message =" Building name must be atleast 5 characters")
+    private String building;
+
+    @NotBlank
+    @Size(min = 4, message =" City name must be atleast 4 characters")
+    private String city;
+
+    @NotBlank
+    @Size(min = 2, message =" Country name must be atleast 4 characters")
+    private String country;
+
+    public Address(String street, String building, String city, String country, String zipcode) {
+        this.street = street;
+        this.building = building;
+        this.city = city;
+        this.country = country;
+        this.zipcode = zipcode;
+    }
+
+    @NotBlank
+    @Size(min = 6, message =" Zipcode name must be atleast 6 characters")
+    private String zipcode;
+
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<>();
+
+
 }
