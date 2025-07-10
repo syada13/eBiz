0b4bdf3e68ea0bc87573b57e738cdceb79fc51e7
Suresh Yadav <suresh23_himcs@rediffmail.com>
2025-07-10 09:41:18 +0530

    Custom UserDetailsService implementation for authentication and authorization

---
+++ src/main/java/com/ecommerce/eCom/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ecommerce.eCom.security.service;
+
+import com.ecommerce.eCom.model.User;
+import com.ecommerce.eCom.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with given username not found: "+username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
