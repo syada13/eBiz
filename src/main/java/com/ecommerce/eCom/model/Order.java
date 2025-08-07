0c07aaf9bf95aed52ca88b28650197a043d46237
Suresh Yadav <suresh23_himcs@rediffmail.com>
2025-08-07 08:38:45 +0530

    Models to manage order

---
+++ src/main/java/com/ecommerce/eCom/model/Order.java
@@ -0,0 +1,48 @@
+package com.ecommerce.eCom.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @OneToMany(mappedBy="order",cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+    private LocalDate orderDate;
+    private Double totalAmount;
+    private String orderStatus;
+
+    @ManyToOne
+    @JoinColumn(name="address_id")
+    private Address  address;
+
+    /*
+    @OneToOne
+    @JoinColumn(name="payment_id")
+    private Payment payment;
+     */
+
+
+}
