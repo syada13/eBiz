f218389005d358b4c7ce2240f5e353be5f771b4c
Suresh Yadav <suresh23_himcs@rediffmail.com>
2025-08-12 11:02:52 +0530

    Fix payment method type length

--- src/main/java/com/ecommerce/eCom/model/Payment.java
+++ src/main/java/com/ecommerce/eCom/model/Payment.java
@@ -22,7 +22,7 @@ public class Payment {
     private Order order;
 
     @NotBlank
-    @Size(min=5, message= "Payment method must contain at least 4 characters.")
+    @Size(min=4, message= "Payment method must contain at least 4 characters.")
     private String paymentMethod;
 
     private String pgPaymentId;
